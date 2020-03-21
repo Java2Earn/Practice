224ecf3297cdd5c14446a773db746e11a887bb97
akashTrainer <52851863+akashTrainer@users.noreply.github.com>
2020-03-21 21:38:39 +0530

    Sample

---
+++ CreateTable/src/CreateEx.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class CreateEx 
+{
+	public static void main(String[] args) throws ClassNotFoundException, SQLException 
+	{
+	//first Step	
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+	//Second step
+	Connection con=	DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","bvrit","bvrit123");
+	System.out.println(	DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","bvrit","bvrit123"));
+System.out.println(con);
+	//third Step
+
+	}
+}
